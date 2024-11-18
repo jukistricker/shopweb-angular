1c6f3d9683c60f27d6d7aa3060a5e0abc6de7e29
jukistricker <thanhthanhbg1@gmail.com>
2024-11-18 21:57:09 +0700

    reup fe

--- src/main.server.ts
+++ src/main.server.ts
@@ -2,7 +2,8 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { OrderComponent } from './app/order/order.component'
 import { AppComponent } from './app/app.component';
 import { ProductDetailComponent } from './app/product-detail/product-detail.component';
+import { OrderConfirmComponent } from './app/order-confirm/order-confirm.component';
 
-const bootstrap = () => bootstrapApplication(ProductDetailComponent);
+const bootstrap = () => bootstrapApplication(OrderConfirmComponent);
 
 export default bootstrap;
--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { OrderComponent } from './app/order/order.component';
 import { AppComponent } from './app/app.component';
 import { ProductDetailComponent } from './app/product-detail/product-detail.component';
+import { OrderConfirmComponent } from './app/order-confirm/order-confirm.component';
 
-bootstrapApplication(ProductDetailComponent)
+bootstrapApplication(OrderConfirmComponent)
   .catch((err) => console.error(err));
